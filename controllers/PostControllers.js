b1db352f7511c68554fab1037e42393510f22ca9
Александр Starkov <starkov.sanderr@yandex.ru>
2023-05-25 23:43:35 +0200

    get all articles

--- controllers/PostControllers.js
+++ controllers/PostControllers.js
@@ -1,5 +1,17 @@
 import PostModel from '../models/Post.js';
 
+export const getAll = async (req, res) => {
+	try {
+		const posts = await PostModel.find();
+		res.json(posts);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: 'failed to get an article',
+		});
+	}
+};
+
 export const create = async (req, res) => {
 	try {
 		const doc = new PostModel({
